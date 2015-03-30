ddc195d99362c2c5e37fc31de44a4c5b2afe4738
Tom Curry <tcurry@ukwebmedia.com>
2015-03-30 12:16:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+todo
+====
+
+A Symfony project created on March 30, 2015, 8:59 am.
---
+++ src/Todo/UserBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tom
+ * Date: 30/03/15
+ * Time: 10:46
+ */
+
+namespace Todo\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser {
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+}
